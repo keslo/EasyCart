51058c5b7f7a5d0313e30ad0027dc72dad2a61ef
Anton Kolesnikov <achallenge86@gmail.com>
2015-07-21 17:39:02 +0400

    Add function addToCart

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ var Cart = function() {
 	var itemBoxClass = 'item_box';
 	var cartContainId = 'cart_content';
 	var itemLockStorName = 'cart';
+	// атрибут для id товара
+	var dataId = 'data_id';
+	// класс с названием товара
+	var itemTitleClass = 'item_title';
+	// класс с ценой товара
+	var itemPriceClass = 'item_price';
 
 	/* Вспомогательные функции */
 
@@ -25,5 +31,37 @@ var Cart = function() {
 
 	/* запись в LocalStorage */
 	function setCartData(data) {
+		localStorage.setItem(JSON.stringify(itemLockStorName, data));
+		return false;
+	};
+
+	function addToCart(el) {
+		// блокируем кнопку на время операции с корзиной
+		this.disabled = true;
+		// получаем данные из LocalStorage или создаем объект
+		var cartData = getCartData() || {};
+		// родительский элемент кнопки "Добавить в корзину"
+		var parentElem = this.parentNode;
+		// id товар
+		var itemId = this.getAttribute(dataId);
+		// название товара
+		var itemTitle = parentNode.querySelector('.' +itemTitleClass).innerHTML;
+		// стоимость товара
+		var itemPrice = parentNode.querySelector('.' +itemPriceClass).innerHTML;
+
+		if (cartData.hasOwnProperty(itemId)) {
+			// если товар к корзине, то плюсуем колличество
+			cartData[itemId][2] += 1;
+		} else {
+			// если товара нет, то добавляем его
+			cartData[itemId] = [itemTitle, itemPrice, 1];
+		};
+
+		// обновляем данные в LocalStorage...
+		if (!setCartData(cartData)) {
+			// ...и если удачно, то разблокируем кнопку
+			this.disabled = false;
+		}
+		return false;
 	}
 }
