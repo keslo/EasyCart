a51e0a162b954ad0833d9a6af3a751e18f50fe00
Anton Kolesnikov <achallenge86@gmail.com>
2015-08-19 19:52:11 +0300

    Delete css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,5 @@ module.export = {
 	entry: './src/js/main.js',
 	output: {
 		filename: './dist/bundle.js'
-	},
-	module: {
-        loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" }
-        ]
-    }
+	}
 }
